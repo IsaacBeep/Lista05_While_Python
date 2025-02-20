44435e2f95445a871bcb73c2efbdcf2393006ab7
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-02-20 20:29:09 -0300

    Numero 1 finalizada

--- EXE01_IsaacSilva.py
+++ EXE01_IsaacSilva.py
@@ -1,9 +1,11 @@
 #Faça um programa que leia os números digitados pelo usuario, a cada número digitado ele deve ser somado ao anterior digitado e a cada soma exibida na tela, quando a soma for superior a 50 deve exibir a mensagem “ O total é... [total] ” e parar o programa.
-numD = int(input('Digite um numero: '))
-numD = int(input('Digite um numero: '))
-soma = numD + numD
-while numD <= 59:
-    soma = numD + numD
+soma = 0 
+while True:
+    numD = int(input('Digite um numero: '))
+    soma += numD
     print('Soma: {}'.format(soma))
-    
-    
+
+    if soma > 50:
+        print("O total é:  {}".format(soma))
+        break 
+print('IsaacSouza')
